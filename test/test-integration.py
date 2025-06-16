093335596861cd4c601d75bad80794f1638fa825
Lars Kotthoff <lars@larsko.org>
2025-06-16 14:15:14 -0600

    clean up tests

--- test/test-integration.py
+++ test/test-integration.py
@@ -14,19 +14,6 @@ def write_conf(path):
     return conf_path
 
 
-def init(shell, local, remote):
-    assert shell.run("cp", "-r", "test/mails", local).returncode == 0
-    assert shell.run("cp", "-r", "test/mails", remote).returncode == 0
-    local_conf = write_conf(local)
-    remote_conf = write_conf(remote)
-    assert shell.run("notmuch", "new", env={"NOTMUCH_CONFIG": local_conf}).returncode == 0
-    assert shell.run("notmuch", "new", env={"NOTMUCH_CONFIG": remote_conf}).returncode == 0
-
-    assert shell.run("notmuch", "count", "*", env={"NOTMUCH_CONFIG": local_conf}).data == 4
-    assert shell.run("notmuch", "count", "*", env={"NOTMUCH_CONFIG": remote_conf}).data == 4
-    return (local_conf, remote_conf)
-
-
 def sync(shell, local_conf, remote_conf):
     res = shell.run("./src/notmuch-sync", "--host", "remote",
                     "--remote-cmd", f"bash -c 'NOTMUCH_CONFIG={remote_conf} ./src/notmuch-sync --host local'",
@@ -35,32 +22,60 @@ def sync(shell, local_conf, remote_conf):
     assert res.returncode == 0
 
 
-def initial_sync(shell, local, local_conf, remote, remote_conf):
-    sync(shell, local_conf, remote_conf)
-    local_sync_file = os.path.join(local, ".notmuch", "notmuch-sync-remote")
-    assert os.path.exists(local_sync_file)
-    with open(local_sync_file, "r", encoding="utf-8") as f:
-        assert re.match('4 [0-9a-z-]+', f.read())
-
-    remote_sync_file = os.path.join(remote, ".notmuch", "notmuch-sync-local")
-    assert os.path.exists(remote_sync_file)
-    with open(remote_sync_file, "r", encoding="utf-8") as f:
-        assert re.match('4 [0-9a-z-]+', f.read())
-
-
 def test_sync_initial(shell):
     with TemporaryDirectory() as local:
         with TemporaryDirectory() as remote:
-            (local_conf, remote_conf) = init(shell, local, remote)
-            initial_sync(shell, local, local_conf, remote, remote_conf)
-            initial_sync(shell, local, local_conf, remote, remote_conf)
+            assert shell.run("cp", "-r", "test/mails", local).returncode == 0
+            assert shell.run("cp", "-r", "test/mails", remote).returncode == 0
+            local_conf = write_conf(local)
+            remote_conf = write_conf(remote)
+            assert shell.run("notmuch", "new", env={"NOTMUCH_CONFIG": local_conf}).returncode == 0
+            assert shell.run("notmuch", "new", env={"NOTMUCH_CONFIG": remote_conf}).returncode == 0
+
+            assert shell.run("notmuch", "count", "*", env={"NOTMUCH_CONFIG": local_conf}).data == 4
+            assert shell.run("notmuch", "count", "*", env={"NOTMUCH_CONFIG": remote_conf}).data == 4
+
+            sync(shell, local_conf, remote_conf)
+
+            local_sync_file = os.path.join(local, ".notmuch", "notmuch-sync-remote")
+            assert os.path.exists(local_sync_file)
+            with open(local_sync_file, "r", encoding="utf-8") as f:
+                assert re.match('4 [0-9a-z-]+', f.read())
+
+            remote_sync_file = os.path.join(remote, ".notmuch", "notmuch-sync-local")
+            assert os.path.exists(remote_sync_file)
+            with open(remote_sync_file, "r", encoding="utf-8") as f:
+                assert re.match('4 [0-9a-z-]+', f.read())
+
+            sync(shell, local_conf, remote_conf)
+            with open(local_sync_file, "r", encoding="utf-8") as f:
+                assert re.match('4 [0-9a-z-]+', f.read())
+            with open(remote_sync_file, "r", encoding="utf-8") as f:
+                assert re.match('4 [0-9a-z-]+', f.read())
 
 
 def test_sync_tags(shell):
     with TemporaryDirectory() as local:
         with TemporaryDirectory() as remote:
-            (local_conf, remote_conf) = init(shell, local, remote)
-            initial_sync(shell, local, local_conf, remote, remote_conf)
+            assert shell.run("cp", "-r", "test/mails", local).returncode == 0
+            assert shell.run("cp", "-r", "test/mails", remote).returncode == 0
+            local_conf = write_conf(local)
+            remote_conf = write_conf(remote)
+            assert shell.run("notmuch", "new", env={"NOTMUCH_CONFIG": local_conf}).returncode == 0
+            assert shell.run("notmuch", "new", env={"NOTMUCH_CONFIG": remote_conf}).returncode == 0
+
+            assert shell.run("notmuch", "count", "*", env={"NOTMUCH_CONFIG": local_conf}).data == 4
+            assert shell.run("notmuch", "count", "*", env={"NOTMUCH_CONFIG": remote_conf}).data == 4
+
+            sync(shell, local_conf, remote_conf)
+            local_sync_file = os.path.join(local, ".notmuch", "notmuch-sync-remote")
+            assert os.path.exists(local_sync_file)
+            with open(local_sync_file, "r", encoding="utf-8") as f:
+                assert re.match('4 [0-9a-z-]+', f.read())
+            remote_sync_file = os.path.join(remote, ".notmuch", "notmuch-sync-local")
+            assert os.path.exists(remote_sync_file)
+            with open(remote_sync_file, "r", encoding="utf-8") as f:
+                assert re.match('4 [0-9a-z-]+', f.read())
 
             assert shell.run("notmuch", "tag", "+local", "id:874llc2bkp.fsf@curie.anarc.at",
                              env={"NOTMUCH_CONFIG": local_conf}).returncode == 0
@@ -95,13 +110,9 @@ def test_sync_tags(shell):
             assert shell.run("notmuch", "search", "--output=tags", "--format=json", "id:87d1dajhgf.fsf@example.net",
                              env={"NOTMUCH_CONFIG": remote_conf}).data == ["remote", "unread"]
 
-            local_sync_file = os.path.join(local, ".notmuch", "notmuch-sync-remote")
-            assert os.path.exists(local_sync_file)
             with open(local_sync_file, "r", encoding="utf-8") as f:
                 assert re.match('10 [0-9a-z-]+', f.read())
 
-            remote_sync_file = os.path.join(remote, ".notmuch", "notmuch-sync-local")
-            assert os.path.exists(remote_sync_file)
             with open(remote_sync_file, "r", encoding="utf-8") as f:
                 assert re.match('10 [0-9a-z-]+', f.read())
 
@@ -109,13 +120,19 @@ def test_sync_tags(shell):
 def test_sync_tags_files(shell):
     with TemporaryDirectory() as local:
         with TemporaryDirectory() as remote:
-            (local_conf, remote_conf) = init(shell, local, remote)
+            assert shell.run("cp", "-r", "test/mails", local).returncode == 0
             Path.unlink(os.path.join(local, "mails", "attachment.eml"))
             Path.unlink(os.path.join(local, "mails", "calendar.eml"))
+            assert shell.run("cp", "-r", "test/mails", remote).returncode == 0
             Path.unlink(os.path.join(remote, "mails", "html-only.eml"))
             Path.unlink(os.path.join(remote, "mails", "simple.eml"))
+            local_conf = write_conf(local)
+            remote_conf = write_conf(remote)
             assert shell.run("notmuch", "new", env={"NOTMUCH_CONFIG": local_conf}).returncode == 0
             assert shell.run("notmuch", "new", env={"NOTMUCH_CONFIG": remote_conf}).returncode == 0
+
+            assert shell.run("notmuch", "count", "*", env={"NOTMUCH_CONFIG": local_conf}).data == 2
+            assert shell.run("notmuch", "count", "*", env={"NOTMUCH_CONFIG": remote_conf}).data == 2
 
             assert shell.run("notmuch", "tag", "+local", "id:87d1dajhgf.fsf@example.net",
                              env={"NOTMUCH_CONFIG": local_conf}).returncode == 0
@@ -154,21 +171,25 @@ def test_sync_tags_files(shell):
             local_sync_file = os.path.join(local, ".notmuch", "notmuch-sync-remote")
             assert os.path.exists(local_sync_file)
             with open(local_sync_file, "r", encoding="utf-8") as f:
-                assert re.match('6 [0-9a-z-]+', f.read())
+                assert re.match('4 [0-9a-z-]+', f.read())
 
             remote_sync_file = os.path.join(remote, ".notmuch", "notmuch-sync-local")
             assert os.path.exists(remote_sync_file)
             with open(remote_sync_file, "r", encoding="utf-8") as f:
-                assert re.match('6 [0-9a-z-]+', f.read())
+                assert re.match('4 [0-9a-z-]+', f.read())
 
 
 def test_sync_tags_files_none_remote(shell):
     with TemporaryDirectory() as local:
         with TemporaryDirectory() as remote:
-            (local_conf, remote_conf) = init(shell, local, remote)
-            shutil.rmtree(os.path.join(remote, "mails"))
+            assert shell.run("cp", "-r", "test/mails", local).returncode == 0
+            local_conf = write_conf(local)
+            remote_conf = write_conf(remote)
             assert shell.run("notmuch", "new", env={"NOTMUCH_CONFIG": local_conf}).returncode == 0
             assert shell.run("notmuch", "new", env={"NOTMUCH_CONFIG": remote_conf}).returncode == 0
+
+            assert shell.run("notmuch", "count", "*", env={"NOTMUCH_CONFIG": local_conf}).data == 4
+            assert shell.run("notmuch", "count", "*", env={"NOTMUCH_CONFIG": remote_conf}).data == 0
 
             assert shell.run("notmuch", "tag", "+local", "id:87d1dajhgf.fsf@example.net",
                              env={"NOTMUCH_CONFIG": local_conf}).returncode == 0
